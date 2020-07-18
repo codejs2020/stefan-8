31874d3eb2e905b6bf3bdafb8699319d279d9967
stefanljusic10 <53339429+stefanljusic10@users.noreply.github.com>
2020-07-18 18:59:05 +0200

    ispravljeno

--- src/zadaci.js
+++ src/zadaci.js
@@ -1,17 +1,17 @@
 // Funkcija za kreiranje tabele pomocu innerhtml-a
 
 function kreiranjeTabele1(brojRedova, brojKolona){
-  let html = "<table>"
+  let html = "<table>";
 
   for (let i=0; i<brojRedova; i++){
-    html += "<tr>"
+    html += "<tr>";
     for (let j=0; j<brojKolona; j++){
-      html += "<td>" + Math.ceil(Math.random()*20) + "</td>"
+      html += "<td>" + Math.ceil(Math.random()*20) + "</td>";
     }
-    html += "</tr>"
+    html += "</tr>";
   }
 
-  html += "</table>"
+  html += "</table>";
 
   document.getElementById("id_tabela1").innerHTML = html;
 }
@@ -25,10 +25,10 @@ function kreiranjeTabele2(brojRedova, brojKolona){
   let newTD;
   let tabela = document.createElement("table");
 
-  for (let i=0; i<brojRedova; i++){
+  for (let i=0; i<brojKolona; i++){
     newTD = document.createElement("td");
 
-    for (let j=0; j<brojKolona; j++){
+    for (let j=0; j<brojRedova; j++){
       newTR = document.createElement("tr");
       newTR.innerHTML = Math.ceil(Math.random()*10);
       newTD.appendChild(newTR);
@@ -40,13 +40,14 @@ function kreiranjeTabele2(brojRedova, brojKolona){
   document.getElementById("id_tabela2").appendChild(tabela);
 }
 
-
+function ispis(){
+  let red = document.getElementById("id_red").value;
+  let kolona = document.getElementById("id_kolona").value;
+  kreiranjeTabele1(red, kolona);
+  kreiranjeTabele2(red, kolona);
+}
 
 // Unos broja redova i kolona i EventListener
 
-let red = document.getElementById("id_red").value;
-let kolona = document.getElementById("id_kolona").value;
 let pozivTabele = document.getElementById("id_unos_tabele");
-
-pozivTabele.addEventListener("click", kreiranjeTabele1(red, kolona));
-pozivTabele.addEventListener("click", kreiranjeTabele2(red, kolona));
+pozivTabele.addEventListener("click", ispis);
