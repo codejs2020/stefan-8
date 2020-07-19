afc3ba0f2c9f284e829ff3eb1b8614eefdd05ef2
stefanljusic10 <53339429+stefanljusic10@users.noreply.github.com>
2020-07-19 15:48:25 +0200

    ispravljeno

--- src/zadaci.js
+++ src/zadaci.js
@@ -1,54 +1,46 @@
 // Funkcija za kreiranje tabele pomocu innerhtml-a
+function kreiranjeTabele1 (brojRedova, brojKolona) {
+  let html = '<table>'
 
-function kreiranjeTabele1(brojRedova, brojKolona){
-  let html = "<table>";
-
-  for (let i=0; i<brojRedova; i++){
-    html += "<tr>";
-    for (let j=0; j<brojKolona; j++){
-      html += "<td>" + Math.ceil(Math.random()*20) + "</td>";
+  for (let i = 0; i < brojRedova; i++) {
+    html += '<tr>'
+    for (let j = 0; j < brojKolona; j++) {
+      html += '<td>' + Math.floor(Math.random() * 10) + '</td>'
     }
-    html += "</tr>";
+    html += '</tr>'
   }
 
-  html += "</table>";
+  html += '</table>'
 
-  document.getElementById("id_tabela1").innerHTML = html;
+  return html
 }
-
-
 
 // Funkcija za Kreiranje tabele pomocu AppendChild
+function kreiranjeTabele2 (brojRedova, brojKolona) {
+  const tabela = document.createElement('table')
 
-function kreiranjeTabele2(brojRedova, brojKolona){
-  let newTR;
-  let newTD;
-  let tabela = document.createElement("table");
+  for (let i = 0; i < brojRedova; i++) {
+    const tr = document.createElement('tr')
 
-  for (let i=0; i<brojKolona; i++){
-    newTD = document.createElement("td");
-
-    for (let j=0; j<brojRedova; j++){
-      newTR = document.createElement("tr");
-      newTR.innerHTML = Math.ceil(Math.random()*10);
-      newTD.appendChild(newTR);
+    for (let j = 0; j < brojKolona; j++) {
+      const td = document.createElement('td')
+      td.innerHTML = Math.floor(Math.random() * 10)
+      tr.appendChild(td)
     }
 
-    tabela.appendChild(newTD);
+    tabela.appendChild(tr)
   }
 
-  document.getElementById("id_tabela2").appendChild(tabela);
+  document.getElementById('tabela2').appendChild(tabela)
 }
 
-function ispis(){
-  // Unos broja redova i kolona i ispis tabela
-  let red = document.getElementById("id_red").value;
-  let kolona = document.getElementById("id_kolona").value;
-  kreiranjeTabele1(red, kolona);
-  kreiranjeTabele2(red, kolona);
+// Unos broja redova i kolona i ispis tabela
+function ispis () {
+  const red = document.getElementById('red').value
+  const kolona = document.getElementById('kolona').value
+  document.getElementById('tabela1').innerHTML = kreiranjeTabele1(red, kolona)
+  kreiranjeTabele2(red, kolona)
 }
 
 // Pozivanje pomocu EventListener-a
-
-let pozivTabele = document.getElementById("id_unos_tabele");
-pozivTabele.addEventListener("click", ispis);
+document.getElementById('unos_tabele').addEventListener('click', ispis)
